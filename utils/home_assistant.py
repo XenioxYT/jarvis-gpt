afd575e7af408e07f6c3af5df6bffcb2c133014d
Xeniox <tomlockwood134@outlook.com>
2023-12-10 21:22:32 +0000

    Refactor toggle_entity function to support turning on and off a switch

--- utils/home_assistant.py
+++ utils/home_assistant.py
@@ -4,21 +4,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def toggle_entity(entity_id):
+def toggle_entity(entity_id, switch):
     access_token = os.getenv('HASS_TOKEN')
-    home_assistant_url=os.getenv('HASS_URL')
+    home_assistant_url = os.getenv('HASS_URL')
     # Determine the type of the entity (e.g., 'switch')
     entity_type = entity_id.split(".")[0]
 
-    # Define the service endpoint based on the entity type
-    # For now, it only supports 'switch'
+    # Define the service endpoint based on the entity type and switch parameter
     if entity_type == "switch":
-        service = "toggle"
+        service = "turn_on" if switch else "turn_off"
     else:
         print(f"Entity type '{entity_type}' not supported yet.")
         return
 
-    # Endpoint to toggle the state of the entity
+    # Endpoint to control the state of the entity
     url = f"{home_assistant_url}/api/services/{entity_type}/{service}"
 
     # Headers including the long-lived access token for authentication
@@ -37,10 +36,12 @@ def toggle_entity(entity_id):
 
     # Check if the request was successful
     if response.status_code == 200:
-        print(f"{entity_id} toggled successfully.")
-        return entity_id + " toggled successfully."
+        action = "turned on" if switch else "turned off"
+        print(f"{entity_id} {action} successfully.")
+        return f"{entity_id} {action} successfully."
     else:
-        print(f"Failed to toggle {entity_id}. Response:", response.text)
+        print(f"Failed to control {entity_id}. Response:", response.text)
+
 
 # Example usage
 # entity_id = "switch.desk_lamp_socket_1" 
