74e6947255714d302e62f2e8265d6e4fe6d20d11
Xeniox <tomlockwood134@outlook.com>
2023-12-11 05:54:26 +0000

    Add common trigrams and bigrams for natural language processing

---
+++ utils/strings.py
@@ -0,0 +1,59 @@
+common_trigrams = [
+    # Smart Home Control
+    ('turn', 'off', 'lights'),
+    ('adjust', 'thermostat', 'to'),
+    ('play', 'next', 'song'),
+
+    # Reminders and Alarms
+    ('set', 'new', 'reminder'),
+    ('cancel', 'current', 'alarm'),
+    ('change', 'reminder', 'time'),
+
+    # Information Queries
+    ('what', 'time', 'is'),
+    ('find', 'recipe', 'for'),
+    ('how', 'to', 'make'),
+
+    # Weather and News
+    ('current', 'weather', 'forecast'),
+    ('latest', 'sports', 'news'),
+    ('update', 'on', 'traffic'),
+
+    # Entertainment
+    ('suggest', 'good', 'movies'),
+    ('play', 'some', 'music'),
+    ('find', 'popular', 'podcasts'),
+
+    # Navigation and Travel
+    ('nearest', 'gas', 'station'),
+    ('best', 'restaurants', 'nearby'),
+    ('public', 'transport', 'schedule'),
+
+    # Personal Assistant Tasks
+    ('send', 'quick', 'email'),
+    ('organize', 'my', 'schedule'),
+    ('book', 'flight', 'tickets'),
+
+    # General Conversational Trigrams
+    ('that', 'sounds', 'great'),
+    ('sure', 'go', 'ahead'),
+    ('not', 'right', 'now'),
+    ('maybe', 'later'),
+    ('that', 'helps', 'alot'),
+    ('could', 'you', 'repeat'),
+    ('did', 'not', 'understand'),
+    ('please', 'clarify', 'that'),
+    ('just', 'wondering'),
+    ('by', 'the', 'way'),
+    ('let', 'me', 'think')
+]
+
+common_bigrams = [
+    # Affirmative Responses
+    ('yes', 'please'),
+    ('thank', 'you'),
+    ('much', 'appreciated'),
+
+    # Negative Responses
+    ('no', 'thanks')
+]
