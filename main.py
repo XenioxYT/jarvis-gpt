869cecac7d2a449802dc32709744b84e1509cdbf
Xeniox <tomlockwood134@outlook.com>
2023-12-13 02:18:05 +0000

    Remove use_flash_attention_2 flag from AutoModelForSpeechSeq2Seq instantiation

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 model_id = "openai/whisper-large-v3"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, use_safetensors=True, use_flash_attention_2=True
+    model_id, torch_dtype=torch_dtype, use_safetensors=True
 )
 model.to(device)
 
