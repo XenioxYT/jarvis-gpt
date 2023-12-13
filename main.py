a44a446b1a48efe2f1c87766a4b6e9fb6893c6a3
Xeniox <tomlockwood134@outlook.com>
2023-12-13 02:02:12 +0000

    Update model_id to use openai/whisper-large-v3

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-model_id = "distil-whisper/distil-medium.en"
+model_id = "openai/whisper-large-v3"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
