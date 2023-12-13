3b7b6efb562d76ea146c7485f7ec404e3280dda4
Xeniox <tomlockwood135@outlook.com>
2023-12-13 04:59:20 +0000

    Commented out function call that was causing an error

--- main.py
+++ main.py
@@ -359,12 +359,12 @@ def get_chatgpt_response(text, function=False, function_name=None):
             function_name = tool_call['function']['name']
             function_args = json.loads(tool_call['function']['arguments'])
             
-            messages.append(
-                {
-                    "role": "assistant",
-                    "content": "You called a function with the following parameters" + function_name + " " + str(function_args),
-                }
-            )
+            # messages.append(
+            #     {
+            #         "role": "assistant",
+            #         "content": "You called a function with the following parameters" + function_name + " " + str(function_args),
+            #     }
+            # )
             store_conversation(1, messages)
 
             print(f"Tool call: {tool_call}")
