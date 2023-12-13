470e90e595376500328f5157e1f33f0140c45f6a
Xeniox <tomlockwood134@outlook.com>
2023-12-13 02:16:36 +0000

    Update model_id in main.py

--- main.py
+++ main.py
@@ -37,10 +37,10 @@ from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-model_id = "distil-whisper/distil-large-v2"
+model_id = "openai/whisper-large-v3"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    model_id, torch_dtype=torch_dtype, use_safetensors=True, use_flash_attention_2=True
 )
 model.to(device)
 
