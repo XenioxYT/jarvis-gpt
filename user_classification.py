a26401f4a35bd1b37e556343da54c8a1976a019e
Xeniox <tomlockwood134@outlook.com>
2023-12-14 04:18:57 +0000

    Refactor user_classification.py: Extracted training and testing code into a separate function

--- user_classification.py
+++ user_classification.py
@@ -12,37 +12,38 @@ labels = []
 features = []
 
 # Extract features from each audio file
-for speaker in os.listdir(dataset_path):
-    speaker_path = os.path.join(dataset_path, speaker)
-    if os.path.isdir(speaker_path):
-        for filename in os.listdir(speaker_path):
-            file_path = os.path.join(speaker_path, filename)
-            if file_path.endswith('.wav'):
-                audio, sr = librosa.load(file_path)
-                mfcc = librosa.feature.mfcc(y=audio, sr=sr)
-                mean_mfcc = np.mean(mfcc, axis=1)
-                features.append(mean_mfcc)
-                labels.append(speaker)
+def train_model():
+    for speaker in os.listdir(dataset_path):
+        speaker_path = os.path.join(dataset_path, speaker)
+        if os.path.isdir(speaker_path):
+            for filename in os.listdir(speaker_path):
+                file_path = os.path.join(speaker_path, filename)
+                if file_path.endswith('.wav'):
+                    audio, sr = librosa.load(file_path)
+                    mfcc = librosa.feature.mfcc(y=audio, sr=sr)
+                    mean_mfcc = np.mean(mfcc, axis=1)
+                    features.append(mean_mfcc)
+                    labels.append(speaker)
 
-# Convert to numpy arrays
-features = np.array(features)
-labels = np.array(labels)
+    # Convert to numpy arrays
+    features = np.array(features)
+    labels = np.array(labels)
 
-# Step 3: Train/Test Split
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+    # Step 3: Train/Test Split
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
-# Step 4: Model Training
-model = MLPClassifier(hidden_layer_sizes=(100,), max_iter=500)
-model.fit(X_train, y_train)
+    # Step 4: Model Training
+    model = MLPClassifier(hidden_layer_sizes=(100,), max_iter=500)
+    model.fit(X_train, y_train)
 
-# Step 5: Testing the Model
-predictions = model.predict(X_test)
-accuracy = accuracy_score(y_test, predictions)
-print(f"Accuracy: {accuracy * 100:.2f}%")
+    # Step 5: Testing the Model
+    predictions = model.predict(X_test)
+    accuracy = accuracy_score(y_test, predictions)
+    print(f"Accuracy: {accuracy * 100:.2f}%")
 
-# Save the model
-model_filename = 'speaker_identification_model.pkl'
-joblib.dump(model, model_filename)
+    # Save the model
+    model_filename = 'speaker_identification_model.pkl'
+    joblib.dump(model, model_filename)
 
 # Function to extract MFCC features
 def extract_features(file_path):
