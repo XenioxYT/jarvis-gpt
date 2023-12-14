be07c33887c5fd378668b86c1520ff9b05554f02
Xeniox <tomlockwood135@outlook.com>
2023-12-14 03:34:18 +0000

    Refactor speaker identification code and add confidence check

--- test.py
+++ test.py
@@ -120,49 +120,59 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
 # Step 2: Data Preparation
-# dataset_path = "./dataset"
-# labels = []
-# features = []
+dataset_path = "./dataset"
+labels = []
+features = []
 
-# # Extract features from each audio file
-# for speaker in os.listdir(dataset_path):
-#     speaker_path = os.path.join(dataset_path, speaker)
-#     if os.path.isdir(speaker_path):
-#         for filename in os.listdir(speaker_path):
-#             file_path = os.path.join(speaker_path, filename)
-#             if file_path.endswith('.wav'):
-#                 audio, sr = librosa.load(file_path)
-#                 mfcc = librosa.feature.mfcc(y=audio, sr=sr)  # Corrected call
-#                 mean_mfcc = np.mean(mfcc, axis=1)
-#                 features.append(mean_mfcc)
-#                 labels.append(speaker)
+# Extract features from each audio file
+for speaker in os.listdir(dataset_path):
+    speaker_path = os.path.join(dataset_path, speaker)
+    if os.path.isdir(speaker_path):
+        for filename in os.listdir(speaker_path):
+            file_path = os.path.join(speaker_path, filename)
+            if file_path.endswith('.wav'):
+                audio, sr = librosa.load(file_path)
+                mfcc = librosa.feature.mfcc(y=audio, sr=sr)
+                mean_mfcc = np.mean(mfcc, axis=1)
+                features.append(mean_mfcc)
+                labels.append(speaker)
 
-# # Convert to numpy arrays
-# features = np.array(features)
-# labels = np.array(labels)
+# Convert to numpy arrays
+features = np.array(features)
+labels = np.array(labels)
 
-# # Step 3: Train/Test Split
-# X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+# Step 3: Train/Test Split
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
-# # Step 4: Model Training
-# model = MLPClassifier(hidden_layer_sizes=(100,), max_iter=500)
-# model.fit(X_train, y_train)
+# Step 4: Model Training
+model = MLPClassifier(hidden_layer_sizes=(100,), max_iter=500)
+model.fit(X_train, y_train)
 
-# # Step 5: Testing the Model
-# predictions = model.predict(X_test)
-# accuracy = accuracy_score(y_test, predictions)
+# Step 5: Testing the Model
+predictions = model.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+print(f"Accuracy: {accuracy * 100:.2f}%")
 
-# model_filename = 'speaker_identification_model.pkl'
-# joblib.dump(model, model_filename)
+# Save the model
+model_filename = 'speaker_identification_model.pkl'
+joblib.dump(model, model_filename)
 
-# print(f"Accuracy: {accuracy * 100:.2f}%")
-
-
+# Function to extract MFCC features
 def extract_features(file_path):
     audio, sr = librosa.load(file_path)
     mfcc = librosa.feature.mfcc(y=audio, sr=sr)
     mean_mfcc = np.mean(mfcc, axis=1)
     return mean_mfcc.reshape(1, -1)
+
+# New function to predict speaker with confidence check
+def predict_speaker(model, features, threshold=0.7):
+    probabilities = model.predict_proba(features)[0]
+    max_prob = np.max(probabilities)
+    predicted_speaker = model.classes_[np.argmax(probabilities)]
+    if max_prob > threshold:
+        return predicted_speaker
+    else:
+        return "Unknown"
 
 # Load the model
 model = joblib.load('speaker_identification_model.pkl')
@@ -172,5 +182,5 @@ new_audio_file = './Recording.wav'
 
 # Extract features and predict
 new_features = extract_features(new_audio_file)
-predicted_speaker = model.predict(new_features)
-print(f"Predicted Speaker: {predicted_speaker[0]}")
+speaker = predict_speaker(model, new_features)
+print(f"Predicted Speaker: {speaker}")
