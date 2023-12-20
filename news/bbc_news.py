694dc0887ec58aa6d353cb831beb967a0e269b4e
Xeniox <Xeniox>
2023-12-20 20:54:17 +0000

    Add audio playback functionality using pyaudio and pydub

--- news/bbc_news.py
+++ news/bbc_news.py
@@ -1,3 +1,4 @@
+import threading
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -5,8 +6,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 import requests
-import pygame
-import threading
+import pyaudio
+import io
+from pydub import AudioSegment
+import time
+import numpy as np
 
 def download_bbc_news_summary():
     # Set up Chrome options
@@ -61,28 +65,48 @@ def download_bbc_news_summary():
         driver.quit()
         return "Downloaded BBC News Summary. It will play after your next response."
 
-def play_mp3(stop_event):
-    # Initialize pygame mixer
-    pygame.mixer.init()
-    # Load the MP3 file
-    pygame.mixer.music.load('./bbc_news_summary.mp3')
-    # Play the MP3 file
-    pygame.mixer.music.play()
-    
-    # While the music is playing, check for the stop event
-    while pygame.mixer.music.get_busy():
+def convert_and_play_mp3(path_to_mp3, stop_event):
+    global current_playback
+
+    # Convert MP3 to WAV using pydub
+    audio = AudioSegment.from_mp3(path_to_mp3)
+    audio = audio.set_frame_rate(24000).set_channels(1)
+    audio_data = np.array(audio.get_array_of_samples(), dtype=np.int16)
+
+    # Apply fade-in to the audio
+    fade_in_duration = 0.2
+    fade_in_samples = int(fade_in_duration * 24000)
+    fade_in_curve = np.linspace(0, 1, fade_in_samples)
+    audio_data[:fade_in_samples] *= fade_in_curve.astype(audio_data.dtype)
+
+    # Convert the numpy array back to bytes
+    modified_audio_content = audio_data.tobytes()
+    audio_buffer = io.BytesIO(modified_audio_content)
+
+    # Callback function to play audio
+    def callback(in_data, frame_count, time_info, status):
         if stop_event.is_set():
-            pygame.mixer.music.stop()  # Stop the music if event is set
-            break
-        pygame.time.delay(100)  # Wait a short time and check again
+            return (None, pyaudio.paComplete)
+        data = audio_buffer.read(frame_count * 2)
+        return (data, pyaudio.paContinue if data else pyaudio.paComplete)
+    
+    # Open a pyaudio stream and start playback
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(2), channels=1, rate=24000, output=True, stream_callback=callback)
+    stream.start_stream()
 
-    pygame.mixer.quit()  # Quit the mixer when done
+    # Keep the stream active until it's stopped or the stop event is set
+    while stream.is_active() and not stop_event.is_set():
+        time.sleep(0.1)
 
-def threaded_playback(path_to_mp3):
-    # Create a thread to play the MP3 file
-    playback_thread = threading.Thread(target=play_mp3, args=(path_to_mp3,))
-    # Start the playback thread
-    playback_thread.start()
+    # Clean up the stream and audio resources
+    stream.stop_stream()
+    stream.close()
+    audio_buffer.close()
+    p.terminate()
 
-# Call the function
-download_bbc_news_summary()
+    # Clear the stop event for the next playback
+    stop_event.clear()
+
+# stop_event = threading.Event()
+# convert_and_play_mp3('bbc_news_summary.mp3', stop_event)
