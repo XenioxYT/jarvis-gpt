a0fdb9dfd1c7fa078eee11b283d9d0dcd2acb25c
Xeniox <Xeniox>
2023-12-26 02:21:59 +0000

    Refactor function and string mappings

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ from utils.send_to_discord import send_message_sync
 from utils.volume_control import volume_up, volume_down
 from utils.notes import save_note, retrieve_notes, edit_or_delete_notes
 from utils.initialise_conversation import initialize_conversation
+from utils.strings import tts_messages, username_mapping, available_functions
 
 
 # Profiling
@@ -419,27 +420,6 @@ def get_chatgpt_response(text, function=False, function_name=None, cursor=None, 
         
     if tool_calls:
 
-        # Dictionary mapping function names to actual function implementations
-        available_functions = {
-            "get_weather_data": get_weather_data,
-            "check_calendar": check_calendar,
-            "set_reminder": add_reminder,
-            "edit_reminder": edit_reminder,
-            "list_reminders": list_unnotified_reminders,
-            "add_event_to_calendar": add_event_to_calendar,
-            "control_switch": toggle_entity,
-            "play_song_on_spotify": search_spotify_song,
-            "enroll_user": enroll_user_handler,
-            "google_search": google_search,
-            "bbc_news_briefing": download_bbc_news_summary,
-            "send_to_phone": send_message_sync,
-            "volume_up": volume_up,
-            "volume_down": volume_down,
-            "save_note": save_note,
-            "retrieve_notes": retrieve_notes,
-            "edit_or_delete_notes": edit_or_delete_notes,
-        }
-
         messages.append(
             {
                 "role": "function",
@@ -447,32 +427,6 @@ def get_chatgpt_response(text, function=False, function_name=None, cursor=None, 
                 "content": str(tool_calls),
             }
         )
-
-        # Map function names to their TTS messages
-        tts_messages = {
-            "play_song_on_spotify": "Connecting to your speakers...",
-            "set_reminder": "Accessing reminders...",
-            "add_event_to_calendar": "Adding event to your calendar...",
-            "get_weather_data": "Getting live weather data...",
-            "check_calendar": "Checking your calendar...",
-            "control_switch": "Controling your smart home...",
-            "edit_reminder": "Editing your reminders...",
-            "list_reminders": "Getting your reminders...",
-            "enroll_user": "Learning your voice...",
-            "google_search": "Searching the web...",
-            "bbc_news_briefing": "Getting the latest news...",
-            "send_to_phone": "Sending a message to your phone...",
-            "volume_up": "Increasing the volume...",
-            "volume_down": "Decreasing the volume...",
-            "save_note": "Saving your note...",
-            "retrieve_notes": "Retrieving your notes...",
-            "edit_or_delete_notes": "Editing your notes...",
-        }
-        
-        username_mapping = {
-            "Tom": "xeniox",
-            "Russell": "russell68"
-        }
 
         multiple_tool_calls = len(tool_calls) > 1
         tts_multiple_spoken = False
