e455e7091201f5a0b485490e7948a3821f2590bc
Xeniox <Xeniox>
2023-12-26 00:15:32 +0000

    Update OAuth redirect URI and save credentials in user-specific folder

--- oauth_server.py
+++ oauth_server.py
@@ -6,14 +6,16 @@ import os
 app = Flask(__name__)
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-REDIRECT_URI = 'https://calendar.xeniox.tv'
+REDIRECT_URI = 'https://calendar.xeniox.tv/oauth2callback'
 
 @app.route('/oauth2callback')
 def oauth2callback():
     code = request.args.get('code', None)
+    state = request.args.get('state', None)
+    user = state.split('_')[1] if state else None
 
-    if not code:
-        return "No authorization code found in request.", 400
+    if not code or not user:
+        return "No authorization code or user found in request.", 400
 
     # Complete the exchange of code for tokens
     flow = Flow.from_client_secrets_file(
@@ -25,7 +27,10 @@ def oauth2callback():
 
     # Save the credentials
     credentials = flow.credentials
-    with open('token.json', 'w') as token_file:
+    # Save the credentials in user-specific folder
+    user_token_dir = f'tokens/{user}'
+    os.makedirs(user_token_dir, exist_ok=True)
+    with open(f'{user_token_dir}/token.json', 'w') as token_file:
         token_file.write(credentials.to_json())
 
     return "Authentication successful. You can close this window.", 200
@@ -35,4 +40,4 @@ def test_route():
     return jsonify({'message': 'Test route is working!'}), 200
 
 if __name__ == '__main__':
-    app.run(ssl_context='adhoc', port=9444)
+    app.run(port=9444, host='0.0.0.0')
