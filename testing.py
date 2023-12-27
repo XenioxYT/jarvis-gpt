dce4982af59b4a251accd5bdf2e4b6bdd1ee1b8d
XenioxYT <tomlockwood135@outlook.com>
2023-12-27 22:49:35 +0000

    testing code refactors for get_chatgpt_response function

--- testing.py
+++ testing.py
@@ -1,182 +1,242 @@
-import datetime
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from googleapiclient.discovery import build
-import os
 import json
-from utils.send_to_discord import send_message_sync
+from openai import OpenAI
+from dotenv import load_dotenv
+from utils.google_search import google_search
+# from utils.tools import tools
+# from utils.strings import avaliable_functions
+import os
+from utils.reminders import add_reminder, edit_reminder, list_unnotified_reminders
 
-SCOPES = ['https://www.googleapis.com/auth/calendar']
+from utils.weather import get_weather_data
 
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+api_base = os.getenv("OPENAI_API_BASE")
 
-def check_token_status():
-    """
-    Checks the status of the user's token.
-    Returns a tuple: (token_is_active, re_auth_link)
-    - token_is_active: Boolean indicating if the token is active.
-    - re_auth_link: String containing the re-authentication link or None if not needed.
-    """
-    creds = None
-    re_auth_link = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+oai_client = OpenAI(base_url=api_base, api_key=api_key)
 
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            re_auth_link = flow.authorization_url()[0]
-
-        return False, re_auth_link
-
-    return True, None
-
-def authenticate_google_calendar_api():
-    token_is_active, re_auth_link = check_token_status()
-    if not token_is_active:
-        if re_auth_link:
-            return re_auth_link
-
-        # The authorization flow must be completed to obtain new credentials
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    else:
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-
-    service = build('calendar', 'v3', credentials=creds)
-    return service
-
-
-def check_calendar(date):
-    """Check the calendar for events on a given date or date range."""
-    service = authenticate_google_calendar_api()
-    # check if the service is a URL using re
-    # if it is, return the URL
-    # else, continue with the rest of the function
-    if "http" in service:
-        send_message_sync("xeniox", "You need to re-auth your google calendar connection. Please visit the following link to do so: " + service)
-        return "The user has been sent a link to their phone to re-auth their google calendar connection: " + service
-    
-    date_range = date.split(" - ")
-    start_date_str = date_range[0]
-    end_date_str = date_range[1] if len(date_range) > 1 else start_date_str
-    try:
-        # Parse dates from strings and create date range for the query
-        time_min = datetime.datetime.fromisoformat(start_date_str).isoformat() + 'Z'
-        time_max = (datetime.datetime.fromisoformat(end_date_str) + datetime.timedelta(days=1)).isoformat() + 'Z'
-
-        # Call the Google Calendar API
-        events_result = service.events().list(calendarId='primary', timeMin=time_min,
-                                              timeMax=time_max, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
-        print(f"Found {len(events)} events")
-
-        # Prepare the list of events in the required output format
-        event_list = [
-            {
-                "summary": event["summary"],
-                "location": event.get("location", "No location specified"),
-                "start": event["start"].get("dateTime", event["start"].get("date")),
-                "end": event["end"].get("dateTime", event["end"].get("date")),
-                "description": event.get("description", "No description provided"),
-                "id": event["id"] if "id" in event else "None",
-                "url": event["htmlLink"] if "htmlLink" in event else "None",
-            } 
-            for event in events
-        ]
-
-        # Output the events for the given date range
-        return json.dumps({"date": date, "events": event_list})
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return json.dumps({"date": date, "error": str(e), "events": []})
-    
-def manage_google_calendar(operation, event_data=None, event_id=None, date=None):
-    service = authenticate_google_calendar_api()
-    if "http" in service:
-        return "The user has been sent a link to their phone to re-auth their google calendar connection: " + service
-    if operation == 'add':
-        event = service.events().insert(calendarId='primary', body=event_data).execute()
-        print(f"Event created: {event.get('htmlLink')}")
-        return "Event created: " + event.get('htmlLink') + " " + event.get('id')
-
-    elif operation == 'remove':
-        if event_id:
-            service.events().delete(calendarId='primary', eventId=event_id).execute()
-            print("Event removed")
-        else:
-            print("No event ID provided to remove an event")
-
-    elif operation == 'list':
-        if date:
-            date_range = date.split(" - ")
-            start_date_str = date_range[0]
-            end_date_str = date_range[1] if len(date_range) > 1 else start_date_str
-            time_min = datetime.datetime.fromisoformat(start_date_str).isoformat() + 'Z'
-            time_max = (datetime.datetime.fromisoformat(end_date_str) + datetime.timedelta(days=1)).isoformat() + 'Z'
-            events_result = service.events().list(calendarId='primary', timeMin=time_min,
-                                                  timeMax=time_max, singleEvents=True,
-                                                  orderBy='startTime').execute()
-            events = events_result.get('items', [])
-            for event in events:
-                print(event['summary'], event['start']['dateTime'], event['end']['dateTime'])
-        else:
-            print("No date provided to list events")
-            
-def add_event_to_calendar(title, start, end, location="None", description="None"):
-    """
-    Adds an event to the Google Calendar with the specified details.
-
-    :param title: The title or summary of the event.
-    :param start: The start date and time of the event in ISO format.
-    :param end: The end date and time of the event in ISO format.
-    :param location: The location of the event (optional).
-    :param description: A description of the event (optional).
-    :return: The event ID of the created event.
-    """
-    service = authenticate_google_calendar_api()
-    if "http" in service:
-        send_message_sync("The user has been sent a link to their phone to re-auth their google calendar connection: " + service)
-        return "The user has been sent a link to their phone to re-auth their google calendar connection: " + service
-
-    # Construct the event dictionary
-    event = {
-        'summary': title,
-        'location': location,
-        'description': description,
-        'start': {
-            'dateTime': start,  # e.g., '2023-07-21T15:00:00-07:00'
-            'timeZone': 'Europe/London',  # This should be changed to the user's preferred time zone
-        },
-        'end': {
-            'dateTime': end,
-            'timeZone': 'Europe/London',  # This should be changed to the user's preferred time zone
-        },
+messages = [
+    {
+        "role": "system",
+        "content": "As ChatGPT, you are a version of ChatGPT that has been optimized for engaging in general conversation, providing informative and accurate responses across a wide range of topics, and maintaining a friendly and approachable demeanor. Your knowledge is up-to-date as of April 2023. You do not have browsing capabilities, but you can process and respond to text inputs, including offering explanations, advice, and creative content. You should always adhere to safe and respectful conversational guidelines."
     }
+]
 
-    # Add optional parameters if they are provided
-    if location!="None":
-        event['location'] = location
-    if description!="None":
-        event['description'] = description
 
-    # Call the Google Calendar API to create the event
-    try:
-        created_event = service.events().insert(calendarId='primary', body=event).execute()
-        print(f"Event created: {created_event.get('htmlLink')}")
-        return "Event created" + created_event.get('htmlLink') + " Title: " + title + " Start: " + start + " End: " + end + " Location: " + location + " Description: " + description + " ID: " + created_event.get('id')
-    except Exception as e:
-        print(f"An error occurred when trying to add the event: {e}")
-        return f"An error occurred when trying to add the event: {e}"
+messages.append({
+    "role": "user",
+    "content": "can you tell me the weather in sheffield and set a reminder for the latest fortnite season?"
+})
+
+
+counter = 0
+
+while True:
+    full_response = ""
+    completion = ""
+    full_completion = ""
+    tool_calls = []
+    available_functions = {
+        "get_weather_data": get_weather_data,
+        # "enroll_user": enroll_user_handler,
+        "google_search": google_search,
+        "set_reminder": add_reminder,
+        "edit_reminder": edit_reminder,
+        "list_reminders": list_unnotified_reminders,
+    }
+    tools = [
+        {
+        "type": "function",
+        "function": {
+            "name": "get_weather_data",
+            "description": "Get the weather data for a specific location and optionally for a particular date or date range using OpenWeatherMap API.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city and country, e.g., York",
+                    },
+                    "date": {
+                        "type": "string",
+                        "description": "An optional date (or date range) to fetch the weather for, e.g., '2023-09-12' or '2023-11-12 - 2023-11-15'. For current weather, this parameter can be omitted. Specify a range for upcoming days. ",
+                    }
+                },
+                "required": ["location"],
+            },
+        }
+    },
+            {
+        "type": "function",
+        "function": {
+            "name": "google_search",
+            "description": "Search Google for a given query and return the top results.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "query": {
+                        "type": "string",
+                        "description": "The query to search for.",
+                    }
+                },
+                "required": ["query"],
+            },
+        },
+    },
+                {
+        "type": "function",
+        "function": {
+            "name": "set_reminder",
+            "description": "Set a reminder for a specified time",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "reminder_text": {
+                        "type": "string",
+                        "description": "The content of the reminder",
+                    },
+                    "reminder_time": {
+                        "type": "string",
+                        "description": "The time for the reminder in format 'YYYY-MM-DD HH:MM'",
+                    },
+                },
+                "required": ["reminder_text", "reminder_time"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "edit_reminder",
+            "description": "Edit an existing reminder",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "search_text": {
+                        "type": "string",
+                        "description": "Natural language text to describe the reminder to be edited",
+                    },
+                    "new_text": {
+                        "type": "string",
+                        "description": "The new content for the reminder (optional)",
+                        "optional": True,
+                    },
+                    "new_time": {
+                        "type": "string",
+                        "description": "The new time for the reminder in format 'YYYY-MM-DD HH:MM' (optional)",
+                        "optional": True,
+                    }
+                },
+                "required": ["search_text"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "list_reminders",
+            "description": "List all reminders that have not yet been notified",
+        },
+    },
+    ]
+    response = oai_client.chat.completions.create(
+        messages=messages,
+        model="gpt-4-1106-preview",
+        tools=tools,
+        stream=True
+    )
+    print("Start of new request\n-----------------------------------------------------------------\n")
+    for chunk in response:
+        # print(chunk)
+        finish_reason = chunk.choices[0].finish_reason
+        print(finish_reason) if finish_reason else None
+        delta = chunk.choices[0].delta
+        full_response += delta.content if delta.content else ""
+
+        if delta.content or delta.content == '':
+            completion += delta.content
+            print(delta.content)
+            full_completion += delta.content
+        # print(completion)
+
+        if delta.tool_calls:
+            tcchunklist = delta.tool_calls
+            for tcchunk in tcchunklist:
+                if len(tool_calls) <= tcchunk.index:
+                    tool_calls.append({"id": "", "type": "function", "function": {"name": "", "arguments": ""}})
+                tc = tool_calls[tcchunk.index]
+
+                if tcchunk.id:
+                    tc["id"] += tcchunk.id
+                if tcchunk.function.name:
+                    tc["function"]["name"] += tcchunk.function.name
+                if tcchunk.function.arguments:
+                    tc["function"]["arguments"] += tcchunk.function.arguments
+    if tool_calls:
+        
+        messages.append({
+            "role": "function",
+            "name": "tool_calls",
+            "content": str(tool_calls)
+        })
+        
+        for tool_call in tool_calls:
+            # print(tool_call)
+            function_name = tool_call["function"]["name"]
+            try:
+                function_args = json.loads(tool_call["function"]["arguments"])
+            except:
+                function_args = {}
+            # print(tool_calls)
+            print(f'\nfunction_name: {function_name}', f'\nfunction_args: {function_args}')
+            
+            if function_name in available_functions:
+                function_response = available_functions[function_name](**function_args)
+                # print(function_response)
+                
+                messages.append({
+                    "role": "function",
+                    "name": function_name,
+                    "content": function_response
+                })
+        
+        if finish_reason == "tool_calls":
+            # print(tool_calls)
+            if full_completion != "":
+                messages.append({
+                    "role": "assistant",
+                    "content": full_completion
+                })
+            print("-------------------------------------------")
+            print(completion)
+            
+            # if counter == 0:
+            #     messages.append({
+            #         "role": "function",
+            #         "name": "get_weather_data",
+            #         "content": "Weather is: 10 degrees"
+            #     })
+            # elif counter == 1:
+            #     messages.append({
+            #         "role": "function",
+            #         "name": "get_weather_data",
+            #         "content": "Weather is: 20 degrees"
+                # })
+            # print(completion)
+            counter += 1
+            full_response = ""
+            full_completion = ""
+            completion = ""
+            print("END OF TOOL CALLS\n-------------------------------------------\n")
+            continue
+            
+    if full_response != "":
+        messages.append({
+            "role": "assistant",
+            "content": full_response
+        })
+        print("END OF RESPONSE\n-------------------------------------------\n")
     
-print(check_calendar("2021-03-01 - 2021-03-31"))
+    if finish_reason == "stop" and len(tool_calls) == 0:
+        print(completion)
+        break
