dc0a2961370f522eb986b13ad376b27671fff154
XenioxYT <tomlockwood135@outlook.com>
2023-12-29 00:51:30 +0000

    Refactor code: Remove unused imports and functions, update dependencies

--- testing.py
+++ testing.py
@@ -1,23 +1,16 @@
 import datetime
-import io
 import json
-import random
 import re
 import threading
-import time
-import numpy as np
 from openai import OpenAI
 from dotenv import load_dotenv
-import pyaudio
 from calendar_utils import add_event_to_calendar, check_calendar
 from news.bbc_news import download_bbc_news_summary
-from noise_reduction import reduce_noise_and_normalize
 from utils.home_assistant import toggle_entity
 from utils.notes import edit_or_delete_notes, retrieve_notes, save_note
 from utils.predict_intent import predict_intent
 from utils.google_search import google_search
 from utils.send_to_discord import send_message_sync
-from google.cloud import texttospeech
 from utils.spotify import search_spotify_song
 # from utils.tools import tools
 from utils.strings import tts_messages, username_mapping
@@ -27,6 +20,7 @@ from utils.store_conversation import store_conversation, get_conversation
 from utils.volume_control import volume_down, volume_up
 from utils.weather import get_weather_data
 from utils.tools import tools
+from text_to_speech import text_to_speech
 # from main import text_to_speech
 
 load_dotenv()
@@ -36,80 +30,6 @@ api_base = os.getenv("OPENAI_API_BASE")
 oai_client = OpenAI(base_url=api_base, api_key=api_key)
 
 tts_thread = None
-
-messages = [
-    {
-        "role": "system",
-        "content": "As ChatGPT, you are a version of ChatGPT that has been optimized for engaging in general conversation, providing informative and accurate responses across a wide range of topics, and maintaining a friendly and approachable demeanor. Your knowledge is up-to-date as of April 2023. You do not have browsing capabilities, but you can process and respond to text inputs, including offering explanations, advice, and creative content. You should always adhere to safe and respectful conversational guidelines."
-    }
-]
-
-client = texttospeech.TextToSpeechClient()
-pa = pyaudio.PyAudio()
-
-
-# Function to convert text to speech using Google Cloud TTS
-def text_to_speech(text):
-    synthesis_input = texttospeech.SynthesisInput(text=text)
-
-    voice = texttospeech.VoiceSelectionParams(
-        language_code='en-GB',
-        name='en-GB-Neural2-B',
-        # ssml_gender=texttospeech.SsmlVoiceGender.MALE
-    )
-
-    # Use audio encoding for high quality
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.LINEAR16
-    )
-
-    # Perform the Text-to-Speech request on the text input with the selected voice parameters and audio file type
-    response = client.synthesize_speech(
-        input=synthesis_input,
-        voice=voice,
-        audio_config=audio_config
-    )
-
-    fade_in_duration = 0.2  # Duration of the fade-in effect in seconds
-    # Convert the audio content to a numpy array and make it writable
-    audio_data = np.frombuffer(response.audio_content, dtype=np.int16).copy()
-    fade_in_samples = int(fade_in_duration * 24000)  # Number of samples over which to apply the fade-in
-    fade_in_curve = np.linspace(0, 1, fade_in_samples, dtype=np.float64)
-
-    # Apply the fade-in effect
-    try:
-        for i in range(fade_in_samples):
-            audio_data[i] = np.int16(float(audio_data[i]) * fade_in_curve[i])
-    except IndexError:
-        pass
-
-    # Convert the audio data back to bytes
-    modified_audio_content = audio_data.tobytes()
-
-    # First, save the modified audio to a buffer
-    audio_buffer = io.BytesIO(modified_audio_content)
-
-    # Then, play the audio buffer using PyAudio
-    # Define PyAudio stream callback for asynchronous playback
-    def callback(in_data, frame_count, time_info, status):
-        return (audio_buffer.read(frame_count * 2), pyaudio.paContinue)
-
-    # Initialize PyAudio and open a stream for playback
-    p = pyaudio.PyAudio()
-    stream = p.open(format=p.get_format_from_width(2),
-                    channels=1,
-                    rate=24000,  # Ensure this matches the sample rate from the TTS response
-                    output=True,
-                    stream_callback=callback)
-
-    # Start the playback stream and wait for it to finish
-    stream.start_stream()
-    while stream.is_active():
-        time.sleep(0.1)  # Pause in the loop to reduce CPU usage
-    stream.stop_stream()
-    stream.close()
-    audio_buffer.close()
-    p.terminate()
 
 
 def split_first_sentence(text):
@@ -332,7 +252,7 @@ def generate_response(input_message, speaker="Unknown", cursor=None, db_conn=Non
             break
         
 
-response, tts_thread = generate_response(input_message="Can you give me a weather update for next week, send it to my phone, and add relevant information to my notes?", speaker="Tom")
+response, tts_thread = generate_response(input_message="Can you edit the second note with more detailed weather information please?", speaker="Tom")
 print(response)
 try:
     if tts_thread.is_alive():
