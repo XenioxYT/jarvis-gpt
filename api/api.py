3ef49629a732167f458b7e2719436e772749fd89
Xeniox <Xeniox>
2023-12-30 21:34:28 +0000

    Add chat endpoint to API

--- api/api.py
+++ api/api.py
@@ -110,6 +110,27 @@ def transcribe_api():
         return jsonify({"transcription": transcription})
 
 
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_message = request.json["message"]
+    # Sending user message to Rasa server
+    rasa_response = requests.post("http://localhost:5005/model/parse", json={"text": user_message})
+    rasa_data = rasa_response.json()
+
+    # Extracting the top intent
+    top_intent = rasa_data.get("intent", {}).get("name")
+    intent_confidence = rasa_data.get("intent", {}).get("confidence")
+
+    # Define a confidence threshold
+    confidence_threshold = 0.60  # You can adjust this threshold
+
+    # Check if the intent is matched with enough confidence
+    if top_intent and intent_confidence > confidence_threshold:
+        return jsonify({"intent": top_intent})
+    else:
+        return jsonify({"intent": "none"})
+
+
 if __name__ == '__main__':
     download_bbc_news_summary()  # Call the function once before starting the Flask application
     scheduler.add_job(id='Scheduled task', func=scheduled_task, trigger='interval', minutes=30)
